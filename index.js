6d4fffe57e0ab68616c07e0357a2b0e74734e87c
Nahian2005 <118044869+Nahian2005@users.noreply.github.com>
2024-08-13 23:47:46 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-var num=prompt("Enter your marks in ICT IN HSC");
+var num = prompt("Enter your marks in ICT IN HSC");
 
-if(num<=100 && num>=80)
+if (num <= 100 && num >= 80)
 
-console.log("Your Grade is: A+");
+    document.write("Your Grade is: A+");
 
- if(num<80 && num>=70)
+if (num < 80 && num >= 70)
 
-console.log("Your Grade is: A");
+    document.write("Your Grade is: A");
 
- if(num<70 && num>=60)
+if (num < 70 && num >= 60)
 
-    console.log("Your Grade is: A-");
-
-
-    if(num<60 && num>=50)
-
-        console.log("Your Grade is: B");
+    document.write("Your Grade is: A-");
 
 
+if (num < 60 && num >= 50)
 
-        if(num<50 && num>=40)
-
-            console.log("Your Grade is: C");
+    document.write ("Your Grade is: B");
 
 
 
-             if(num<40 && num>=33)
+if (num < 50 && num >= 40)
 
-                console.log("Your Grade is: D");
+    document.write("Your Grade is: C");
 
 
-                if(num<33 && num>=0)
 
-                    console.log("F***! You have Failed! Go eat GRASS IN YOUR HOME!");
+if (num < 40 && num >= 33)
+
+    document.write("Your Grade is: D");
+
+
+if (num < 33 && num >= 0)
+
+    document.write("F***! You have Failed! Go eat GRASS IN YOUR HOME!");
 
