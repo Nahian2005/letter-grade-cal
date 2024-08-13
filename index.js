bc6d8102196b527fe0db20a9654a6511fcfd661b
Nahian2005 <118044869+Nahian2005@users.noreply.github.com>
2024-08-14 00:40:05 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,37 +4,39 @@ if(num > 100 || num < 0)
 document.write("Salar pot. Aj projonto kono exam e dekchis kauke 100 r beshi ba 0 er kom paite? HAla. VAG");
     
 
-if (num <= 100 && num >= 80)
+else if (num <= 100 && num >= 80)
 
     document.write("Your Grade is: A+");
 
-if (num < 80 && num >= 70)
+
+else if (num < 80 && num >= 70)
 
     document.write("Your Grade is: A");
 
-if (num < 70 && num >= 60)
+else if (num < 70 && num >= 60)
 
     document.write("Your Grade is: A-");
 
 
-if (num < 60 && num >= 50)
+else if (num < 60 && num >= 50)
 
     document.write ("Your Grade is: B");
 
 
 
-if (num < 50 && num >= 40)
+else if (num < 50 && num >= 40)
 
     document.write("Your Grade is: C");
 
 
 
-if (num < 40 && num >= 33)
+else if (num < 40 && num >= 33)
 
     document.write("Your Grade is: D");
 
 
-if (num < 33 && num >= 0)
+else
 
     document.write("F***! You have Failed! Go eat GRASS IN YOUR HOME!");
+
 
