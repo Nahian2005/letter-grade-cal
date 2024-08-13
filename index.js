fdd2a34b49540076c1df7d985f70d35459479142
Nahian2005 <118044869+Nahian2005@users.noreply.github.com>
2024-08-13 14:02:06 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+var num=prompt("Enter your marks in ICT IN HSC");
+
+if(num<=100 && num>=80)
+
+console.log("Your Grade is: A+");
+
+ if(num<80 && num>=70)
+
+console.log("Your Grade is: A");
+
+ if(num<70 && num>=60)
+
+    console.log("Your Grade is: A-");
+
+
+    if(num<60 && num>=50)
+
+        console.log("Your Grade is: B");
+
+
+
+        if(num<50 && num>=40)
+
+            console.log("Your Grade is: C");
+
+
+
+             if(num<40 && num>=33)
+
+                console.log("Your Grade is: D");
+
+
+                if(num<33 && num>=0)
+
+                    console.log("F***! You have Failed! Go eat GRASS IN YOUR HOME!");
+
